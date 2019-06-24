32ffe7361e0440783e554e0955bb34f23391ebc4
zmatthews33 <zmatt33@gmail.com>
2019-06-24 11:29:18 -0500

    build

---
+++ client/build/precache-manifest.8593a44fbf452c4cf6a73916b27d911c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27bc5c74e57f69faf42d",
+    "url": "/static/css/main.065dfe67.chunk.css"
+  },
+  {
+    "revision": "27bc5c74e57f69faf42d",
+    "url": "/static/js/main.e3bd4c61.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fb3046d2bde1d8e620ea",
+    "url": "/static/js/2.078bd7ae.chunk.js"
+  },
+  {
+    "revision": "41a0078e6a6559fac96c5ab64ee5f878",
+    "url": "/static/media/books.41a0078e.jpg"
+  },
+  {
+    "revision": "5e4279c3432da1406fe6e7a33b2bdec1",
+    "url": "/index.html"
+  }
+];
